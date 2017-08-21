53c10f10ca6012bcf19657c9c19d330ed621eee7
Dauchez <dauchezguillaume@yahoo.fr>
2017-08-21 12:41:45 +0200

    Index.php Création d'un second personnage

--- index.php
+++ index.php
@@ -9,3 +9,6 @@ spl_autoload_register('chargerClasse');
 
 $perso = new personnage(personnage::FORCE_MOYENNE);
 personnage::parler();
+
+$perso2 = new personnage(personnage::FORCE_MOYENNE);
+personnage::parler();
